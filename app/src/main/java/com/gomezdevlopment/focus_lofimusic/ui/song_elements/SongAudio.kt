7cfea7923695d4506fb36d0eee2fd9cc16263884
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-17 18:45:45 -0400

    Add more songs and create logic for skipping to next and previous songs

--- app/src/main/java/com/gomezdevlopment/focus_lofimusic/ui/song_elements/SongAudio.kt
+++ app/src/main/java/com/gomezdevlopment/focus_lofimusic/ui/song_elements/SongAudio.kt
@@ -3,12 +3,25 @@ package com.gomezdevlopment.focus_lofimusic.ui.song_elements
 
 const val bedtimeAfterACoffee = "https://d3cda8tr8wfro2.cloudfront.net/barradeen-bedtime-after-a-coffee.mp3"
 const val blossom = "https://d3cda8tr8wfro2.cloudfront.net/blossom.mp3"
+const val distant = "https://d3cda8tr8wfro2.cloudfront.net/distant.mp3"
 const val embrace = "https://d3cda8tr8wfro2.cloudfront.net/embrace.mp3"
+const val ethereal = "https://d3cda8tr8wfro2.cloudfront.net/ethereal.mp3"
 const val fluid = "https://d3cda8tr8wfro2.cloudfront.net/fluid.mp3"
+const val fragile = "https://d3cda8tr8wfro2.cloudfront.net/fragile.mp3"
 const val herbalTea = "https://d3cda8tr8wfro2.cloudfront.net/herbal-tea.mp3"
+const val interplanetaryTrip = "https://d3cda8tr8wfro2.cloudfront.net/interplanetary-trip.mp3"
+const val jazzaddictsIntro = "https://d3cda8tr8wfro2.cloudfront.net/jazzaddicts-intro.mp3"
 const val lofiStudy = "https://d3cda8tr8wfro2.cloudfront.net/lofiStudy.mp3"
+const val missingTheStreet = "https://d3cda8tr8wfro2.cloudfront.net/missing-the-street.mp3"
+const val missingYou = "https://d3cda8tr8wfro2.cloudfront.net/missing-you.mp3"
+const val noTurningBack = "https://d3cda8tr8wfro2.cloudfront.net/no-turning-back.mp3"
+const val odyssey = "https://d3cda8tr8wfro2.cloudfront.net/odyssey.mp3"
+const val reverse = "https://d3cda8tr8wfro2.cloudfront.net/reverse.mp3"
 const val sandCastles = "https://d3cda8tr8wfro2.cloudfront.net/purrple-cat-sand-castles.mp3"
 const val smores = "https://d3cda8tr8wfro2.cloudfront.net/purrple-cat-smores.mp3"
+const val sorrow = "https://d3cda8tr8wfro2.cloudfront.net/sorrow.mp3"
+const val sunShinesThroughTheLeaves = "https://d3cda8tr8wfro2.cloudfront.net/sun-shines-through-the-leaves.mp3"
+const val underwater = "https://d3cda8tr8wfro2.cloudfront.net/underwater.mp3"
 
 
 const val sandCastlesCredits = "Sand Castles by Purrple Cat | https://purrplecat.com/\n" +
@@ -30,3 +43,46 @@ const val herbalTeaCredits = "herbal tea by Artificial.Music | https://soundclou
         "Licensed under Creative Commons: Attribution 3.0 Unported (CC BY 3.0)\n" +
         "https://creativecommons.org/licenses/by/3.0/\n" +
         "Music promoted by https://www.chosic.com/free-music/all/ "
+
+const val distantCredits = "Distant by Ghostrifter Official | https://soundcloud.com/ghostrifter-official\n" +
+        "Creative Commons — Attribution-NoDerivs 3.0 Unported — CC BY-ND 3.0\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/ "
+
+const val etherealCredits = "Ethereal by Ghostrifter Official | https://soundcloud.com/ghostrifter-official\n" +
+        "Creative Commons — Attribution-NoDerivs 3.0 Unported — CC BY-ND 3.0\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/"
+
+const val fragileCredits = "Fragile by Keys of Moon | https://soundcloud.com/keysofmoon\n" +
+        "Attribution 4.0 International (CC BY 4.0)\n" +
+        "https://creativecommons.org/licenses/by/4.0/\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/ "
+
+const val jazzaddictsIntroCredits = "Jazzaddict’s Intro by Cosimo Fogg (201) | https://soundcloud.com/cosimo-fogg\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Creative Commons Attribution 3.0 Unported License\n" +
+        "https://creativecommons.org/licenses/by/3.0/ "
+
+const val missingYouCredits = "Missing You by Purrple Cat | https://purrplecat.com/\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Creative Commons CC BY-SA 3.0\n" +
+        "https://creativecommons.org/licenses/by-sa/3.0/"
+
+const val underwaterCredits = "Underwater by LiQWYD | https://soundcloud.com/liqwyd/\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Licensed under Creative Commons: Attribution 3.0 Unported (CC BY 3.0)\n" +
+        "https://creativecommons.org/licenses/by/3.0/"
+
+const val sorrowCredits = "Sorrow by Sappheiros | https://soundcloud.com/sappheirosmusic\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Creative Commons CC BY 3.0\n" +
+        "https://creativecommons.org/licenses/by/3.0/"
+
+const val reverseCredits = "Reverse by Uniq | https://soundcloud.com/uniqofficial/\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Attribution 4.0 International (CC BY 4.0)\n" +
+        "https://creativecommons.org/licenses/by/4.0/"
+
+const val sunShinesThroughTheLeavesCredits = "Sun shines through the leaves by Babasmas | https://soundcloud.com/babasmasmoosic\n" +
+        "Music promoted by https://www.chosic.com/free-music/all/\n" +
+        "Creative Commons CC BY-SA 3.0\n" +
+        "https://creativecommons.org/licenses/by-sa/3.0/"
--- app/src/main/java/com/gomezdevlopment/focus_lofimusic/viewModels/MusicPlayerViewModel.kt
+++ app/src/main/java/com/gomezdevlopment/focus_lofimusic/viewModels/MusicPlayerViewModel.kt
@@ -21,22 +21,19 @@ import coil.request.SuccessResult
 import com.gomezdevlopment.focus_lofimusic.R
 import com.gomezdevlopment.focus_lofimusic.models.Song
 import com.gomezdevlopment.focus_lofimusic.ui.song_elements.*
-import com.gomezdevlopment.focus_lofimusic.ui.theme.Purple40
-import com.gomezdevlopment.focus_lofimusic.ui.theme.Purple80
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 
 class MusicPlayerViewModel(private val context: Context) : ViewModel() {
     val sliderValue: MutableState<Int> = mutableStateOf(0)
     val songIsPlaying: MutableState<Boolean> = mutableStateOf(false)
-    var mediaPlayer: MediaPlayer = MediaPlayer()
-    var mediaPlayer2: MediaPlayer = MediaPlayer()
-    var currentPlayer: MutableState<MediaPlayer> = mutableStateOf(mediaPlayer)
-    var queuedPlayer: MutableState<MediaPlayer> = mutableStateOf(mediaPlayer2)
+    var previousPlayer: MutableState<MediaPlayer> = mutableStateOf(MediaPlayer())
+    var currentPlayer: MutableState<MediaPlayer> = mutableStateOf(MediaPlayer())
+    var queuedPlayer: MutableState<MediaPlayer> = mutableStateOf(MediaPlayer())
     var queuedPlayerIsPrepared: MutableState<Boolean> = mutableStateOf(false)
+    var previousPlayerIsPrepared: MutableState<Boolean> = mutableStateOf(false)
     var currentPlaylistIndex by mutableStateOf(0)
     var songArtBitmap: MutableState<Bitmap> =
         mutableStateOf(BitmapFactory.decodeResource(context.resources, R.drawable.embrace_art))
@@ -44,12 +41,25 @@ class MusicPlayerViewModel(private val context: Context) : ViewModel() {
     var playlist: List<Song> = listOf(
         Song(bedtimeAfterACoffee, bedtimeAfterACoffeeArt, "bedtime after a coffee", "Barradeen", bedtimeAfterACoffeeCredits),
         Song(blossom, blossomArt, "Spirit Blossom", "RomanBelov", "RomanBelov"),
+        Song(distant, distantArt, "Distant", "Ghostrifter Official", distantCredits),
         Song(embrace, embraceArt, "Embrace", "ItsWatR", "ItsWatR"),
+        Song(ethereal, etherealArt, "Ethereal", "Ghostrifter Official", etherealCredits),
         Song(fluid, fluidArt, "Fluid", "ItsWatR", "ItsWatR"),
+        Song(fragile, fragileArt, "Fragile", "Keys of Moon", fragileCredits),
         Song(herbalTea, herbalTeaArt, "Herbal Tea", "Artificial.Music", herbalTeaCredits),
+        Song(interplanetaryTrip, interplanetaryTripArt, "Interplanetary Trip", "Billy Wuot", "Billy Wuot"),
+        Song(jazzaddictsIntro, jazzaddictsIntroArt, "Jazzaddict’s Intro", "Cosimo Fogg", jazzaddictsIntroCredits),
         Song(lofiStudy, lofiStudyArt, "Lofi Study", "FASSounds", "FASSounds"),
+        Song(missingTheStreet, missingTheStreetArt, "Missing The Street", "Billy Wuot", "Billy Wuot"),
+        Song(missingYou, missingYouArt, "Missing You", "Purrple Cat", missingYouCredits),
+        Song(noTurningBack, noTurningBackArt, "No Turning Back", "Billy Wuot", "Billy Wuot"),
+        Song(odyssey, odysseyArt, "Odyssey", "Billy Wuot", "Billy Wuot"),
+        Song(reverse, reverseArt, "Reverse", "Uniq", reverseCredits),
         Song(sandCastles, sandCastlesArt, "Sand Castles", "Purrple Cat", sandCastlesCredits),
-        Song(smores, smoresArt, "S\'mores", "Purrple Cat", smoresCredits),
+        Song(sorrow, sorrowArt, "Sorrow", "Sappheiros", sorrowCredits),
+        Song(sunShinesThroughTheLeaves, sunShinesThroughTheLeavesArt, "Sun shines through the leaves", "Babasmas", sunShinesThroughTheLeavesCredits),
+        Song(underwater, underwaterArt, "Underwater", "LiQWYD", underwaterCredits),
+        Song(smores, smoresArt, "S'mores", "Purrple Cat", smoresCredits),
     )
 
     var currentSongLength = mutableStateOf(60f)
@@ -106,49 +116,63 @@ class MusicPlayerViewModel(private val context: Context) : ViewModel() {
     }
 
     fun nextSong() {
-        resetMediaPlayer()
+        resetMediaPlayer(true)
         if (currentPlaylistIndex < playlist.lastIndex)
             currentPlaylistIndex += 1
         else
             currentPlaylistIndex = 0
         loadSongArt()
-        if(queuedPlayerIsPrepared.value){
+        if (queuedPlayerIsPrepared.value) {
             currentPlayer.value.start()
-        }else{
+            queuedPlayerIsPrepared.value = false
+        } else {
             songIsPlaying.value = false
         }
     }
 
     fun previousSong() {
-        resetMediaPlayer()
+        resetMediaPlayer(false)
         if (currentPlaylistIndex > 0)
             currentPlaylistIndex -= 1
         else
             currentPlaylistIndex = playlist.lastIndex
+        loadSongArt()
+        if (previousPlayerIsPrepared.value) {
+            currentPlayer.value.start()
+            previousPlayerIsPrepared.value = false
+        } else {
+            songIsPlaying.value = false
+        }
     }
 
-    private fun resetMediaPlayer() {
+    private fun resetMediaPlayer(next: Boolean) {
         timer?.cancel()
         currentPlayer.value.pause()
         currentPlayer.value.reset()
         currentPlayer.value.release()
-        val finishedPlayer = currentPlayer.value
-        currentPlayer.value = queuedPlayer.value
+        if (next) {
+            currentPlayer.value = queuedPlayer.value
+        } else {
+            currentPlayer.value = previousPlayer.value
+        }
         currentSongLength.value = currentPlayer.value.duration.toFloat()
         createTimer()
-        queuedPlayer.value = finishedPlayer
         queuedPlayer.value = MediaPlayer()
-        createQueuedPlayer()
+        previousPlayer.value = MediaPlayer()
+        createQueuedPlayers()
     }
 
     private fun createMediaPlayer() {
         try {
-            mediaPlayer.setAudioAttributes(AUDIO_ATTRIBUTES)
-            mediaPlayer.setDataSource(context, Uri.parse(playlist[currentPlaylistIndex].audioUrl))
-            mediaPlayer.prepare()
+            currentPlayer.value.setAudioAttributes(AUDIO_ATTRIBUTES)
+            currentPlayer.value.setDataSource(
+                context,
+                Uri.parse(playlist[currentPlaylistIndex].audioUrl)
+            )
+            currentPlayer.value.prepare()
 
-            mediaPlayer.setOnPreparedListener {
-                createQueuedPlayer()
+            currentPlayer.value.setOnPreparedListener {
+                createQueuedPlayers()
                 currentSongLength.value = currentPlayer.value.duration.toFloat()
                 createTimer()
             }
@@ -159,17 +183,44 @@ class MusicPlayerViewModel(private val context: Context) : ViewModel() {
 
     }
 
-    private fun createQueuedPlayer() {
+    private fun createQueuedPlayers() {
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 queuedPlayer.value.setAudioAttributes(AUDIO_ATTRIBUTES)
-                if (currentPlaylistIndex < playlist.lastIndex)
-                    queuedPlayer.value.setDataSource(context, Uri.parse(playlist[currentPlaylistIndex+1].audioUrl))
-                else
+
+                if (currentPlaylistIndex < playlist.lastIndex) {
+                    queuedPlayer.value.setDataSource(
+                        context,
+                        Uri.parse(playlist[currentPlaylistIndex + 1].audioUrl)
+                    )
+                } else {
                     queuedPlayer.value.setDataSource(context, Uri.parse(playlist[0].audioUrl))
+                }
+
                 queuedPlayer.value.prepare()
                 queuedPlayer.value.setOnPreparedListener {
+                    println("next prepared")
                     queuedPlayerIsPrepared.value = true
+                }
+
+                previousPlayer.value.setAudioAttributes(AUDIO_ATTRIBUTES)
+
+                if (currentPlaylistIndex > 0) {
+                    previousPlayer.value.setDataSource(
+                        context,
+                        Uri.parse(playlist[currentPlaylistIndex - 1].audioUrl)
+                    )
+                } else {
+                    previousPlayer.value.setDataSource(
+                        context,
+                        Uri.parse(playlist[playlist.lastIndex].audioUrl)
+                    )
+                }
+
+                previousPlayer.value.prepare()
+                previousPlayer.value.setOnPreparedListener {
+                    println("prev prepared")
+                    previousPlayerIsPrepared.value = true
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -197,7 +248,10 @@ class MusicPlayerViewModel(private val context: Context) : ViewModel() {
 
     private fun createTimer() {
         timer = object :
-            CountDownTimer(currentPlayer.value.duration.toLong() - currentPlayer.value.currentPosition, 1000) {
+            CountDownTimer(
+                currentPlayer.value.duration.toLong() - currentPlayer.value.currentPosition,
+                1000
+            ) {
             override fun onTick(millisUntilFinished: Long) {
                 sliderValue.value = currentPlayer.value.currentPosition
             }
